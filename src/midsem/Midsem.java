a5f0cc2a4d645193db9d25891b55fcb74db95101
ss <nizarfa@sheridancollege.ca>
2023-02-21 18:12:19 -0500

    I Am Done Message

---
+++ src/midsem/Midsem.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package midsem;
+
+/**
+ * SYST 17796 Software Design Fundamentals
+ * @author Fadil Nizar
+ */
+
+     
+
+public class Midsem {
+    public static void main(String args[]) {
+
+        Car car = new Car();
+        Bike bike = new Bike();
+        Boat boat = new Boat();
+
+        Vehicle[] racers = { car, bike, boat };
+
+        for (Vehicle x : racers) {
+            x.go();
+        }
+
+    }
+}
+
+    }
+
+}
